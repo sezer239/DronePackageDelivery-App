65cc472249572c1f9c286dcc2963202f60b438c3
kadir <kadirtas02@gmail.com>
2019-12-03 18:14:18 +0300

    repaired

--- app/src/main/java/com/example/dronepackagedelivery_app/fragments/ShopFragment.java
+++ app/src/main/java/com/example/dronepackagedelivery_app/fragments/ShopFragment.java
@@ -59,39 +59,99 @@ public class ShopFragment extends Fragment implements
         TabLayout tabLayout = v.findViewById(R.id.tab_layout);
 
 
-        ProductFragment foodProdFrag = ProductFragment.newInstance(2,"Food",mCartRef);
-        ProductFragment drinkProdFrag = ProductFragment.newInstance(2,"Drink",mCartRef);
-        ProductFragment firutProdFrag = ProductFragment.newInstance(2,"Firut",mCartRef);
-        ProductFragment asdProdFrag = ProductFragment.newInstance(1,"ASD",mCartRef);
-        ProductFragment cccProdFrag = ProductFragment.newInstance(2,"CCC",mCartRef);
-        ProductFragment buyukFoodFrag = ProductFragment.newInstance(1,"BUYUK FOOD",mCartRef);
-        ProductFragment mediumFoodFrag = ProductFragment.newInstance(2,"MEDUIM FOOD",mCartRef);
-        ProductFragment kucukYemekFrag = ProductFragment.newInstance(2,"KUCUK YEMEK",mCartRef);
+        ProductFragment frag1 = ProductFragment.newInstance(2,"Meyve & Sebze",mCartRef);
+        ProductFragment frag2 = ProductFragment.newInstance(2,"Unlu Mamüller",mCartRef);
+        ProductFragment frag3 = ProductFragment.newInstance(2,"Atıştırmalık",mCartRef);
+        ProductFragment frag4 = ProductFragment.newInstance(1,"Dondurma",mCartRef);
+        ProductFragment frag5 = ProductFragment.newInstance(2,"Yiyecek",mCartRef);
+        ProductFragment frag6 = ProductFragment.newInstance(1,"İçecek",mCartRef);
+        ProductFragment frag7 = ProductFragment.newInstance(2,"Fit & Form",mCartRef);
+        ProductFragment frag8 = ProductFragment.newInstance(2,"Süt & Kahvaltı",mCartRef);
+        ProductFragment frag9 = ProductFragment.newInstance(2,"Temel Gıda",mCartRef);
+        ProductFragment frag10 = ProductFragment.newInstance(2,"Kişisel Bakım",mCartRef);
+        ProductFragment frag11 = ProductFragment.newInstance(2,"Ev & Temizlik",mCartRef);
+        ProductFragment frag12 = ProductFragment.newInstance(1,"Ev & Yaşam",mCartRef);
+        ProductFragment frag13 = ProductFragment.newInstance(2,"Teknoloji",mCartRef);
+        ProductFragment frag14 = ProductFragment.newInstance(1,"Evcil Hayvan",mCartRef);
+        ProductFragment frag15 = ProductFragment.newInstance(2,"Bebek",mCartRef);
+        ProductFragment frag16 = ProductFragment.newInstance(2,"Cinsel Sağlık",mCartRef);
+        ProductFragment frag17 = ProductFragment.newInstance(2,"Giyim",mCartRef);
 
-        fragmentPagerAdapter.addPage(foodProdFrag);
-        fragmentPagerAdapter.addPage(drinkProdFrag);
-        fragmentPagerAdapter.addPage(firutProdFrag);
-        fragmentPagerAdapter.addPage(asdProdFrag);
-        fragmentPagerAdapter.addPage(cccProdFrag);
-        fragmentPagerAdapter.addPage(buyukFoodFrag);
-        fragmentPagerAdapter.addPage(kucukYemekFrag);
-        fragmentPagerAdapter.addPage(mediumFoodFrag);
-
+        fragmentPagerAdapter.addPage(frag1);
+        fragmentPagerAdapter.addPage(frag2);
+        fragmentPagerAdapter.addPage(frag3);
+        fragmentPagerAdapter.addPage(frag4);
+        fragmentPagerAdapter.addPage(frag5);
+        fragmentPagerAdapter.addPage(frag6);
+        fragmentPagerAdapter.addPage(frag7);
+        fragmentPagerAdapter.addPage(frag8);
+        fragmentPagerAdapter.addPage(frag9);
+        fragmentPagerAdapter.addPage(frag10);
+        fragmentPagerAdapter.addPage(frag11);
+        fragmentPagerAdapter.addPage(frag12);
+        fragmentPagerAdapter.addPage(frag13);
+        fragmentPagerAdapter.addPage(frag14);
+        fragmentPagerAdapter.addPage(frag15);
+        fragmentPagerAdapter.addPage(frag16);
+        fragmentPagerAdapter.addPage(frag17);
 
         vpPager.setAdapter(fragmentPagerAdapter);
         tabLayout.setupWithViewPager(vpPager);
         fragmentPagerAdapter.notifyDataSetChanged();
 
-        Log.d("HHH" , "argCAT "+ mArgCategory);
-        if(mArgCategory.equals("Meyve & Sebze")){
-            tabLayout.getTabAt(0).select();
-        }else if(mArgCategory.equals("Unlu Mamüller")){
-            Log.d("HHH" , "UNLU MAMUL");
-            tabLayout.getTabAt(1).select();
-        }else if(mArgCategory.equals("Yiyecek")){
-            tabLayout.getTabAt(2).select();
+        switch(mArgCategory) {
+            case "Meyve & Sebze":
+                tabLayout.getTabAt(0).select();
+                break;
+            case "Unlu Mamüller":
+                tabLayout.getTabAt(1).select();
+                break;
+            case "Atıştırmalık":
+                tabLayout.getTabAt(2).select();
+                break;
+            case "Dondurma":
+                tabLayout.getTabAt(3).select();
+                break;
+            case "Yiyecek":
+                tabLayout.getTabAt(4).select();
+                break;
+            case "İçeçek":
+                tabLayout.getTabAt(5).select();
+                break;
+            case "Fit & Form":
+                tabLayout.getTabAt(6).select();
+                break;
+            case "Süt & Kahvaltı":
+                tabLayout.getTabAt(7).select();
+                break;
+            case "Temel Gıda":
+                tabLayout.getTabAt(8).select();
+                break;
+            case "Kişisel Bakım":
+                tabLayout.getTabAt(9).select();
+                break;
+            case "Ev & Temizlik":
+                tabLayout.getTabAt(10).select();
+                break;
+            case "Ev & Yaşam":
+                tabLayout.getTabAt(11).select();
+                break;
+            case "Teknoloji":
+                tabLayout.getTabAt(12).select();
+                break;
+            case "Evcil Hayvan":
+                tabLayout.getTabAt(13).select();
+                break;
+            case "Bebek":
+                tabLayout.getTabAt(14).select();
+                break;
+            case "Cinsel Sağlık":
+                tabLayout.getTabAt(15).select();
+                break;
+            case "Giyim":
+                tabLayout.getTabAt(16).select();
+                break;
         }
-
         return v;
     }
 
